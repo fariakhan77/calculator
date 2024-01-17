8f80bae1821ab2b1f4670f156f82a91ce60f391d
fariakhan77 <156866563+fariakhan77@users.noreply.github.com>
2024-01-17 03:23:47 -0800

    Add files via upload

---
+++ faria khan.py
@@ -0,0 +1,11 @@
+num1=int(input("enter no 1"))
+operator=int(input("enter an operator 3"))
+num2=int(input("enter no 3"))
+if operator =="+":
+         print(num1+num2)
+elif operator =="-":
+        print(operator-num1)
+elif operator =="/":
+        print(num2/operator)
+elif operator =="*":
+        print(operator*num1)
